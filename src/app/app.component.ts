a9b7a36b753733807401d047bf98a9ec42b6c2af
Oresu <javierusero@outlook.com>
2023-10-19 19:18:29 +0200

    hola adios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ht-management';
+  employeeForm: FormGroup = new FormGroup({});
+
+  constructor(private _fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.setEmpForm();
+  }
+
+  setEmpForm() {
+    this.employeeForm = this._fb.group({
+      id: [0],
+      department: ['', Validators.required],
+      empName: ['', Validators.required],
+      mobile: ['', Validators.required],
+      gender: ['', Validators.required],
+      joinDate: ['', Validators.required],
+      email: ['', Validators.required],
+      salary: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPass: ['', Validators.required],
+      empStatus: [false, Validators.requiredTrue],
+    })
+  }
+
+  formSubmit(){
+    console.log(this.employeeForm.value);
+  }
+
+  get f(){
+    return this.employeeForm.controls;
+  }
 }
